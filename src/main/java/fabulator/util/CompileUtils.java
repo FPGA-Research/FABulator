8b13098d788103908f2ea919cd3410a13e2a172d
_Trizion_ <jakobternes@gmail.com>
2024-03-22 17:31:20 +0100

    Change output method

--- src/main/java/fabulator/util/CompileUtils.java
+++ src/main/java/fabulator/util/CompileUtils.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
@@ -89,8 +88,8 @@ public class CompileUtils {
                 TimeUnit.MILLISECONDS
         );
 
-        System.out.println(Arrays.toString(compileToFasmCmd));
         ProcessBuilder processBuilder = new ProcessBuilder(compileToFasmCmd);
+        processBuilder.command().forEach(System.out::println);
         processBuilder.start();
     }
 
