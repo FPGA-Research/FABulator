26af17a8a938c32d1ba09d92f4e167c223f0a22f
_Trizion_ <jakobternes@gmail.com>
2025-08-01 00:28:54 +0200

    Change medium LOD threshold

--- src/main/java/fabulator/ui/view/fabric/LodManager.java
+++ src/main/java/fabulator/ui/view/fabric/LodManager.java
@@ -18,7 +18,7 @@ public class LodManager {
 
     public enum Lod {
         LOW(0.15), // for Bels (Rect -> Rects)
-        MEDIUM(1.2), // for Wires (Rect -> Lines)
+        MEDIUM(0.5), // for Wires (Rect -> Lines)
         HIGH(1.7); // for Ports (Line -> Circles)
 
         @Getter
